f684641e6e89904079dc3028b2363e7e37d2b1b5
PiyushD30 <133135876+PiyushD30@users.noreply.github.com>
2023-06-14 02:23:04 +0530

    Update crowdfunding.sol

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -4,12 +4,12 @@ contract crowdFunding {
     address payable public admin;
     uint public goalAmount = 100 ether;
     uint public totalAmount = 0;
-    mapping(address => uint) public pay;
+    mapping(address => uint) public person_paid;
     address[] person;
     uint public people_no = 0;
     uint start_time;
     uint public time;
-
+    
     constructor(uint _goalAmount, uint _start_time) {
         admin = payable(msg.sender);
         goalAmount = _goalAmount;
@@ -21,29 +21,55 @@ contract crowdFunding {
         require(msg.sender == admin, "Only the admin can access!");
         _;
     }
+  
+    modifier goalNotReached(){
+        require(totalAmount < goalAmount,"Goal Reached");
+        _;
+    }
     
-    function contribute() public payable {
+    modifier campaignIsOpen(){
+        require(time < 2 days,"Campaign Closed!");
+        _;
+    }
+    
+    function contribute(uint amount) public payable goalNotReached campaignIsOpen {
+        amount=msg.value;
+        uint t=0;
+        uint i=0;
         time = block.timestamp-start_time;
-        require(totalAmount < goalAmount, "Goal Reached!");
-        require(time < 2 days , "Campaign Closed!");
-        person.push(msg.sender);
-        pay[person[people_no]] = msg.value;
-        totalAmount += msg.value;
-        people_no++;
-    }
-    
-    function refund() public {
-        require(totalAmount < goalAmount);
-        require(time > 2 days);
-        for(uint i=0; i<people_no;i++){
-            payable(person[i]).transfer(pay[person[i]]);
+        require(amount <= goalAmount,"Too High Amount!");
+        for(i=0; i<people_no;i++){
+            if(person[i]==msg.sender){
+                t=1;
+                break;
+            }
         }
+        if(t==0){
+        person.push(msg.sender);
+        person_paid[person[people_no]] +=amount;
+        totalAmount += amount;
+        people_no++;
+        } 
+        else{
+        person_paid[person[i]] += amount;
+        totalAmount += amount;
+        }  
     }
     
-    function closeCampaign() public onlyAdmin {
-        if (goalAmount == totalAmount) {
-            admin.transfer(totalAmount);
-            totalAmount = 0;
+    function refund() public payable goalNotReached {
+        require(time > 2 days,"Campaign is Ongoing.");
+        for(uint i=0; i<people_no;i++){
+            payable(person[i]).transfer(person_paid[person[i]]);
+            person_paid[person[i]]=0;
+        }
+        totalAmount=0;
+    }
+    
+    function transfer_fund(uint transfer_amount) public payable onlyAdmin {
+        if (goalAmount >= totalAmount && transfer_amount<= totalAmount) {
+            totalAmount -= transfer_amount;
+            payable(admin).transfer(transfer_amount);
         }
     }
 }
+//SPDX-License-Identifier: UNLICENSED
